d59e3925f0cb2e4f16b5e2380be469b89ac6dc86
liliya.galina <liliya.galina@neadevis.com>
2019-07-24 19:05:33 +0300

    Style dfst vghscatter chart

--- src/app/components/_utils/charts/scatter-chart/scatter-chart.component.ts
+++ src/app/components/_utils/charts/scatter-chart/scatter-chart.component.ts
@@ -12,12 +12,18 @@ export class ScatterChartComponent implements OnInit {
   mainColor = '#809400';
   markerConfiguration = ScatterMarkerOptions;
   tooltipConfiguration = ScatterTooltipOptions;
+  categoriesMap = new Map<number, string>([
+    [0, ''], [1, 'Flexible thinking'], [2, 'Performance under preasure'],
+    [3, 'Strategic thinking'], [4, 'Planning'], [5, 'Eficiency'],
+    [6, 'Analytical ability'], [7, 'Quick thinking'], [8, '']
+  ]);
 
   xAxeLabelsConfig = {
-    // content: (e) => {
-    //   debugger;
-    //   return null;
-    // },
+    content: (e) => {
+      return this.categoriesMap.get(e.value).replace(' ', '\n');
+    },
+    font: `font-family: 'Open Sans', sans-serif; font-size: 20px; `,
+
   };
 
   xAxisCategories = {
@@ -29,7 +35,7 @@ export class ScatterChartComponent implements OnInit {
     { position: 1, score: 66, display: 'sfdsf' },
     { position: 2, score: 76, display: 'sfdsf' },
     { position: 4, score: 90, display: 'sfdsf' },
-    { position: 4, score: 50, display: 'sfdsf' }]  ];
+    { position: 4, score: 50, display: 'sfdsf' }]];
 
   constructor() { }
 
